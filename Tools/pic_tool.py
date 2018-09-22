bdee183d9bf34292c19140c2bff2ba49c03d2990
illusion_ <thortwo@outlook.com>
2018-09-22 22:00:47 +0800

    linux test

--- Tools/pic_tool.py
+++ Tools/pic_tool.py
@@ -4,6 +4,7 @@ import os
 from peewee import *
 
 db = MySQLDatabase("station", host='127.0.0.1', port=3339, user='station', passwd='123456', charset='utf8')
+# db = MySQLDatabase("station", host='127.0.0.1', port=3306, user='root', passwd='123456', charset='utf8')
 
 
 class DomainConf(Model):
