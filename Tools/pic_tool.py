53757c5659842eaaa330ca46ff35cc44d1bbff01
illusion_ <thortwo@outlook.com>
2018-09-22 21:56:20 +0800

    linux test

--- Tools/pic_tool.py
+++ Tools/pic_tool.py
@@ -2,7 +2,20 @@ from PIL import Image, ImageDraw, ImageFont
 from random import choice
 import os
 from peewee import *
-from Tools.lib.DomainConf import DomainConf
+
+db = MySQLDatabase("station", host='127.0.0.1', port=3306, user='station', passwd='123456', charset='utf8')
+
+
+class DomainConf(Model):
+    domain = CharField(max_length=255, unique=True)
+    Template = CharField(max_length=255, default='CPModel_2')
+    index_title = CharField(max_length=255)
+    index_keywords = TextField()
+    index_description = TextField()
+
+    class Meta:
+        database = db
+        table_name = 'CPModel_2_domainconf'
 
 
 class ImgTool:
